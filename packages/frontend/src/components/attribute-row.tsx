319a383d9921a881a9ff6d1b145c904ce79a8fa7
David Dorward <david@dorward.me.uk>
2021-07-11 16:31:50 +0100

    Improve icons for attributes

--- packages/frontend/src/components/attribute-row.tsx
+++ packages/frontend/src/components/attribute-row.tsx
@@ -1,31 +1,42 @@
-
-import React, { useState, useEffect } from 'react';
-import { Attribute, UserData } from '../types';
-import { H2, H3, HTMLTable, Icon } from '@blueprintjs/core';
+import React from 'react';
+import { Attribute } from '../types';
+import { Icon } from '@blueprintjs/core';
 import { attributeValues } from '../consts';
 
-
-export type RowProps = { 
-	attribute: Attribute 
+export type RowProps = {
+	attribute: Attribute;
 };
-
 
 const AttributeRow = ({ attribute }: RowProps) => {
 	console.log({ attribute });
 	const attributeValue = attributeValues.filter(value => value[0] === attribute.value)[0][1];
 
-	return (<tr>
+	return (
+		<tr>
 			<td>
 				<label>
 					<input type="checkbox" name={attribute.name} value="use" /> {attribute.name}
 				</label>
 			</td>
-            {attributeValues.map(([_name, score]) => {
-                const available = score <= attributeValue;
-    	        return <td>{available ? <Icon icon="tick-circle" /> : ""}</td>;
+			{attributeValues.map(([_name, score]) => {
+				if (score < attributeValue) {
+					return (
+						<td>
+							<Icon icon="tick-circle" />
+						</td>
+					);
+				}
+				if (score === attributeValue) {
+					return (
+						<td>
+							<Icon icon="tick-circle" color="green" />
+						</td>
+					);
+				}
+				return <td><Icon icon="ban-circle" color="#BFCCD6" /></td>;
 			})}
 		</tr>
 	);
 };
 
-export default AttributeRow
+export default AttributeRow;
