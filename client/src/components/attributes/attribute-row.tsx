537e07c9c4279f04585d3ac27a88f6baf03abe9c
David Dorward <david@dorward.me.uk>
2023-01-29 12:06:58 +0000

    Adjust play mode for mobile

--- client/src/components/attributes/attribute-row.tsx
+++ client/src/components/attributes/attribute-row.tsx
@@ -4,6 +4,7 @@ import { attributeValues } from '../../consts';
 import { selectSelected, toggleSelected } from '../../data/roll-slice';
 import { useDispatch, useSelector } from 'react-redux';
 import AttributeState from './attribute-state';
+import SimpleHealth from './simple-health';
 
 import classnames from 'classnames';
 
@@ -13,16 +14,21 @@ const AttributeRow = ({ attribute, character, isWoundable }: RowProps) => {
 
 	const firstBought = attributeValues.findIndex(value => value[0] === attribute.value);
 	const firstUnwounded = firstBought + attribute.wounds;
-
 	const checked = !!selectedAttributes[attribute.id];
 	const onChange = () => dispatch(toggleSelected(attribute.id));
 
 	return (
-		<tr key={attribute.name} className={classnames({ wounded: Boolean(attribute.wounds) })}>
+		<tr key={attribute.name} className={classnames({ attributeRow: true, wounded: Boolean(attribute.wounds) })}>
 			<td key="label" className="bonus-name">
 				<Checkbox checked={checked} onChange={onChange}>
 					{attribute.name}
 				</Checkbox>
+				<SimpleHealth
+					characterId={character.id}
+					attributeId={attribute.id}
+					firstUnwounded={firstUnwounded}
+					firstBought={firstBought}
+				/>
 			</td>
 			{attributeValues.map(([name], index) => (
 				<td key={name}>
