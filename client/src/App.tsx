8863aaf94a4f67ff351b9460fb2acaee1ca42a66
David Dorward <david@dorward.me.uk>
2023-07-06 19:34:45 +0100

    Update blueprint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,13 @@
-import './index.scss';
-import '@blueprintjs/core/lib/css/blueprint.css';
-import '@blueprintjs/icons/lib/css/blueprint-icons.css';
-import '@blueprintjs/popover2/lib/css/blueprint-popover2.css';
-import 'normalize.css';
-
-import { Provider } from 'react-redux';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Dashboard from './components/dashboard';
 import Home from './components/home';
 import Login from './components/login';
-
 import store from './data/redux-store';
+import './index.scss';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+import 'normalize.css';
+import { Provider } from 'react-redux';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 const App: React.FC = () => {
 	return (
--- client/src/components/character-header/index.tsx
+++ client/src/components/character-header/index.tsx
@@ -1,12 +1,11 @@
 import { selectEditingCharacter } from '../../data/edit-mode-slice';
-import { useSelector } from 'react-redux';
 import CharacterHeaderEdit from './character-header-edit';
-import CharacterHeaderView from './character-header-view';
 import type { Props } from './character-header-types';
+import CharacterHeaderView from './character-header-view';
+import { useSelector } from 'react-redux';
 
 const CharacterHeader = (props: Props) => {
 	const characterToEdit = useSelector(selectEditingCharacter);
-	console.log('character header', { characterToEdit });
 	if (characterToEdit) {
 		return <CharacterHeaderEdit {...props} />;
 	}
