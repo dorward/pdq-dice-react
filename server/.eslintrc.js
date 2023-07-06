23b697a770daee962f6d495c5e35949781146eac
David Dorward <david@dorward.me.uk>
2023-07-06 19:16:09 +0100

    Refactor eslint configuration.

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,27 +1,25 @@
+/* eslint-env node */
 module.exports = {
-	root: true,
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint', 'import'],
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'prettier',
-		'plugin:import/recommended',
-		'plugin:import/typescript',
-	],
-	rules: {
-		'sort-imports': [
-			'error',
-			{
-				ignoreCase: false,
-				ignoreDeclarationSort: false,
-				ignoreMemberSort: false,
-				memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-				allowSeparatedGroups: false,
-			},
-		],
-	},
-	'import/parsers': {
-		'@typescript-eslint/parser': ['.ts', '.tsx'],
-	},
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'import'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+    ],
+    rules: {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+            'error',
+            {
+                vars: 'all',
+                args: 'after-used',
+                destructuredArrayIgnorePattern: '^_',
+                ignoreRestSiblings: false,
+            },
+        ],
+    },
 };
