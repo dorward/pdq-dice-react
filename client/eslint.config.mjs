77566b0b394a1e302edb1448ce2cd5b8671cedb7
David Dorward <david@dorward.me.uk>
2024-11-18 20:06:16 +0000

    Tidy up

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,25 +1,3 @@
-// import eslint from "@eslint/js";
-// import tseslint from "typescript-eslint";
-// // import eslintConfigPrettier from "eslint-config-prettier";
-// // import importPlugin from "eslint-plugin-import";
-
-// export default tseslint.config(
-//   // {
-//   //   files: ["**/*.ts", "**/*.tsx"],
-//   // },
-//   eslint.configs.recommended,
-//   tseslint.configs.recommended
-//   // importPlugin.flatConfigs.recommended,
-//   // eslintConfigPrettier,
-//   // {
-//   //   files: ["**/*.ts", "**/*.tsx"],
-
-//   //   rules: {
-//   //     "no-var": "error",
-//   //   },
-//   // }
-// );
-
 import eslint from '@eslint/js';
 import ts_eslint from 'typescript-eslint';
 
