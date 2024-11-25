7bf4028f56defe1663174cff33b1206045d7cb32
David Dorward <david@dorward.me.uk>
2024-11-25 14:01:22 +0000

    Fix: Inventory items can now be rolled
    The code was still looking in the old extras tree

--- client/src/api/roll.ts
+++ client/src/api/roll.ts
@@ -8,122 +8,129 @@ import { SkillCheckRequestBody } from '../types';
 import axios from 'axios';
 
 const getBase = () => {
-	const API_URL = process.env.API_URL;
-	const whoami = selectWhoami(store.getState());
-	const characterId = selectCharacterId(store.getState());
+    const API_URL = process.env.API_URL;
+    const whoami = selectWhoami(store.getState());
+    const characterId = selectCharacterId(store.getState());
 
-	const url = `${API_URL}roll/${whoami.userId}/${whoami.code}/`;
-	const auth = { characterId };
-	return { auth, url };
+    const url = `${API_URL}roll/${whoami.userId}/${whoami.code}/`;
+    const auth = { characterId };
+    return { auth, url };
 };
 
 type D6Params = {
-	high?: boolean;
+    high?: boolean;
 };
 
 export const d6 = async ({ high }: D6Params) => {
-	store.dispatch(markLoading());
-	const [rollType, description] =
-		typeof high === 'undefined' ? ['d6', undefined] : [`Luck roll`, high ? 'High' : 'Low'];
-	const { auth, url } = getBase();
-	const data = {
-		...auth,
-		dice: '1d6',
-		high,
-		rollType,
-		description,
-	};
-	const response = await axios.post(url, data);
-	const result = response.data.result;
-	store.dispatch(setResult(response.data));
-	return result;
+    store.dispatch(markLoading());
+    const [rollType, description] =
+        typeof high === 'undefined' ? ['d6', undefined] : [`Luck roll`, high ? 'High' : 'Low'];
+    const { auth, url } = getBase();
+    const data = {
+        ...auth,
+        dice: '1d6',
+        high,
+        rollType,
+        description,
+    };
+    const response = await axios.post(url, data);
+    const result = response.data.result;
+    store.dispatch(setResult(response.data));
+    return result;
 };
 
 type SkillCheckProps = {
-	isUsingBenny?: boolean;
+    isUsingBenny?: boolean;
 };
 
 type SimpleSkillCheckProps = {
-	description: string;
-	modifier: number;
+    description: string;
+    modifier: number;
 };
 
 export const simpleSkillCheck = async ({ description, modifier }: SimpleSkillCheckProps) => {
-	const { auth, url } = getBase();
+    const { auth, url } = getBase();
 
-	const body: SkillCheckRequestBody = {
-		dice: '2d6',
-		bonuses: [
-			{
-				name: 'Modifiers',
-				value: modifier,
-			},
-		],
-		description,
-		rollType: 'Skill Check',
-	};
-	const response = await axios.post(url, { ...auth, ...body });
-	return response.data;
+    const body: SkillCheckRequestBody = {
+        dice: '2d6',
+        bonuses: [
+            {
+                name: 'Modifiers',
+                value: modifier,
+            },
+        ],
+        description,
+        rollType: 'Skill Check',
+    };
+    const response = await axios.post(url, { ...auth, ...body });
+    return response.data;
 };
 
 export const skillCheck = async ({ isUsingBenny }: SkillCheckProps = {}) => {
-	let body: SkillCheckRequestBody;
+    let body: SkillCheckRequestBody;
 
-	if (isUsingBenny) {
-		body = store.getState().lastRoll.roll;
-	} else {
-		const { selected, description, circumstance } = store.getState().roll;
-		store.dispatch(markLoading());
-		const character = selectCharacter(store.getState());
-		const qualityBonuses = [...character.qualities, ...character.powers]
-			.filter(attribute => selected[attribute.id])
-			.map(attribute => {
-				const value =
-					attributeValues[
-						attributeValues.findIndex(value => value[0] === attribute.value) + (attribute.wounds || 0)
-					][1];
-				return {
-					name: attribute.name,
-					value,
-				};
-			});
-		const extraBonuses = character.extras
-			.filter(extra => selected[extra.id] && extra.count !== 0)
-			.map(extra => ({
-				name: extra.name,
-				value: extra.value,
-			}));
-		character.extras
-			.filter(extra => selected[extra.id] && !countsThatDoNotReduce.includes(extra.count))
-			.forEach(extra => {
-				store.dispatch(spendInventoryItem({ characterId: character.id, extraId: extra.id }));
-			});
-		const circumstanceBonus = circumstance.value ? [{ name: circumstance.name, value: circumstance.value }] : [];
+    if (isUsingBenny) {
+        body = store.getState().lastRoll.roll;
+    } else {
+        const { selected, description, circumstance } = store.getState().roll;
+        store.dispatch(markLoading());
+        const character = selectCharacter(store.getState());
+        const qualityBonuses = [...character.qualities, ...character.powers]
+            .filter((attribute) => selected[attribute.id])
+            .map((attribute) => {
+                const value =
+                    attributeValues[
+                        attributeValues.findIndex((value) => value[0] === attribute.value) +
+                            (attribute.wounds || 0)
+                    ][1];
+                return {
+                    name: attribute.name,
+                    value,
+                };
+            });
+        const extraBonuses = character.inventory
+            .filter((item) => selected[item.id] && item.count !== 0)
+            .map((item) => ({
+                name: item.name,
+                value: item.value,
+            }));
+        character.inventory
+            .filter((item) => selected[item.id] && !countsThatDoNotReduce.includes(item.count))
+            .forEach((item) => {
+                store.dispatch(
+                    spendInventoryItem({ characterId: character.id, extraId: item.id }),
+                );
+            });
+        const circumstanceBonus = circumstance.value
+            ? [{ name: circumstance.name, value: circumstance.value }]
+            : [];
 
-		const bonuses = [...qualityBonuses, ...extraBonuses, ...circumstanceBonus];
+        const bonuses = [...qualityBonuses, ...extraBonuses, ...circumstanceBonus];
 
-		body = {
-			dice: '2d6',
-			bonuses,
-			description,
-			rollType: 'Skill Check',
-		};
-		store.dispatch(updateLastRoll(body));
-	}
+        body = {
+            dice: '2d6',
+            bonuses,
+            description,
+            rollType: 'Skill Check',
+        };
+        store.dispatch(updateLastRoll(body));
+    }
 
-	const { auth, url } = getBase();
-	const data = { ...auth, ...body };
+    const { auth, url } = getBase();
+    const data = { ...auth, ...body };
 
-	try {
-		const response = await axios.post(url, data);
-		const {
-			data: { result },
-		} = response;
-		store.dispatch(setResult(response.data));
-		return result;
-	} catch (error) {
-		console.error({ error });
-		store.dispatch(markClear());
-		store.dispatch(setUserError('There was an unexpected error. Try logging in using Discord again.'));
-	}
+    try {
+        const response = await axios.post(url, data);
+        const {
+            data: { result },
+        } = response;
+        store.dispatch(setResult(response.data));
+        return result;
+    } catch (error) {
+        console.error({ error });
+        store.dispatch(markClear());
+        store.dispatch(
+            setUserError('There was an unexpected error. Try logging in using Discord again.'),
+        );
+    }
 };
