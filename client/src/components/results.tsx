118f9f9322c6385ad223216b7ca6f38d2a681050
David Dorward <david@dorward.me.uk>
2024-12-02 18:30:22 +0000

    Autofocus the roll dialog
    This means it can be dismissed with enter instead of playing dice poke

--- client/src/components/results.tsx
+++ client/src/components/results.tsx
@@ -10,65 +10,69 @@ import { useDispatch, useSelector } from 'react-redux';
 import Loading from './loading';
 
 const Results = () => {
-	const dispatch = useDispatch();
-	const results = useSelector(selectResults);
-	const benniesAvailable = useSelector(selectBennyUse);
-	const characterId = useSelector(selectCharacterId);
-	return (
-		<Overlay
-			autoFocus
-			enforceFocus
-			onClose={() => {
-				/* Clear state */
-			}}
-			isOpen={!!(results.loading || results.roll)}>
-			<div className="v-center">
-				<Card interactive={true} elevation={1} className="h-center results">
-					{results.loading && <Loading />}
-					{results?.roll?.results && (
-						<>
-							<div>
-								<h2>Result: {results.roll.total}</h2>
-								<HTMLTable>
-									<tbody>
-										{results.roll.results.map(result => (
-											<tr key={result.name}>
-												<td>{result.name}</td>
-												<td>{result.value}</td>
-											</tr>
-										))}
-									</tbody>
-								</HTMLTable>
-							</div>
-							<ButtonGroup>
-								<Button
-									onClick={() => {
-										dispatch(markRollClear());
-										dispatch(markClear());
-									}}
-									intent={Intent.PRIMARY}>
-									OK
-								</Button>
+    const dispatch = useDispatch();
+    const results = useSelector(selectResults);
+    const benniesAvailable = useSelector(selectBennyUse);
+    const characterId = useSelector(selectCharacterId);
+    return (
+        <Overlay
+            autoFocus
+            enforceFocus
+            onClose={() => {
+                /* Clear state */
+            }}
+            isOpen={!!(results.loading || results.roll)}
+        >
+            <div className="v-center">
+                <Card interactive={true} elevation={1} className="h-center results">
+                    {results.loading && <Loading />}
+                    {results?.roll?.results && (
+                        <>
+                            <div>
+                                <h2>Result: {results.roll.total}</h2>
+                                <HTMLTable>
+                                    <tbody>
+                                        {results.roll.results.map((result) => (
+                                            <tr key={result.name}>
+                                                <td>{result.name}</td>
+                                                <td>{result.value}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </HTMLTable>
+                            </div>
+                            <ButtonGroup>
+                                <Button
+                                    autoFocus
+                                    onClick={() => {
+                                        dispatch(markRollClear());
+                                        dispatch(markClear());
+                                    }}
+                                    intent={Intent.PRIMARY}
+                                >
+                                    OK
+                                </Button>
 
-								{benniesAvailable ? (
-									<Button
-										onClick={() => {
-											dispatch(useBenny());
-											dispatch(spendBenny(characterId));
-											skillCheck({ isUsingBenny: true });
-										}}
-										className="bennyButton"
-										intent={Intent.NONE}>
-										Benny ({benniesAvailable})
-									</Button>
-								) : null}
-							</ButtonGroup>
-						</>
-					)}
-				</Card>
-			</div>
-		</Overlay>
-	);
+                                {benniesAvailable ? (
+                                    <Button
+                                        onClick={() => {
+                                            dispatch(useBenny());
+                                            dispatch(spendBenny(characterId));
+                                            skillCheck({ isUsingBenny: true });
+                                        }}
+                                        className="bennyButton"
+                                        intent={Intent.NONE}
+                                    >
+                                        Benny ({benniesAvailable})
+                                    </Button>
+                                ) : null}
+                            </ButtonGroup>
+                        </>
+                    )}
+                </Card>
+            </div>
+        </Overlay>
+    );
 };
 
 export default Results;
