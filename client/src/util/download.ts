bba9d428d2b56d2c2f234a9070accc5a5279d7e7
David Dorward <david@dorward.me.uk>
2025-03-20 13:36:17 +0000

    Fix export
    A bug in handling null values made the removing ID function fail.
    
    Since preserving IDs is required for the new version of the inventory’s Location feature, it has been stripped out entirely and the next task is regenerate them on import.

--- client/src/util/download.ts
+++ client/src/util/download.ts
@@ -2,48 +2,38 @@ import yaml from 'js-yaml';
 import type { Character } from '../types';
 
 type SaveData = {
-	filename: string;
-	data: string;
-	contentType?: string;
+    filename: string;
+    data: string;
+    contentType?: string;
 };
 
 type SaveYaml = {
-	filename: string;
-	data: Character;
+    filename: string;
+    data: Character;
 };
 
 export const save = ({ filename, data, contentType = 'text/plain' }: SaveData) => {
-	const blob = new Blob([data], { type: contentType });
-	const element = window.document.createElement('a');
-	const objectURL = window.URL.createObjectURL(blob);
-	element.href = objectURL;
-	element.download = filename;
-	document.body.appendChild(element);
-	element.click();
-	setTimeout(() => {
-		document.body.removeChild(element);
-		window.URL.revokeObjectURL(objectURL);
-	}, 50);
+    const blob = new Blob([data], { type: contentType });
+    const element = window.document.createElement('a');
+    const objectURL = window.URL.createObjectURL(blob);
+    element.href = objectURL;
+    element.download = filename;
+    document.body.appendChild(element);
+    element.click();
+    setTimeout(() => {
+        document.body.removeChild(element);
+        window.URL.revokeObjectURL(objectURL);
+    }, 50);
 };
 
 export const saveYaml = ({ filename, data }: SaveYaml) => {
-	const text = yaml.dump(data);
-	save({ filename, data: text, contentType: 'text/yaml' });
+    const text = yaml.dump(data);
+    save({ filename, data: text, contentType: 'text/yaml' });
 };
 
-export const saveCharacterAsYaml = (internal: Character) => {
-	const character = removeIds(internal) as Character;
-	saveYaml({ filename: `${character.name || character.codeName || 'Unnamed Character'}.yml`, data: character });
-};
-
-const removeIds = (input: unknown): unknown => {
-	if (Array.isArray(input)) return input.map(removeIds);
-	if (typeof input === 'object')
-		return Object.entries(input).reduce((accumulator, currentValue): Record<string, unknown> => {
-			const [key, value] = currentValue;
-			if (key === 'id') return accumulator;
-			accumulator[key] = removeIds(value);
-			return accumulator;
-		}, {} as Record<string, unknown>);
-	return input;
+export const saveCharacterAsYaml = (character: Character) => {
+    saveYaml({
+        filename: `${character.name || character.codeName || 'Unnamed Character'}.yml`,
+        data: character,
+    });
 };
