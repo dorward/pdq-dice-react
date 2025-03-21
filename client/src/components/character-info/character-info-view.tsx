6c8ba97fb43091ff32ebb98b472d22f68c5e0b53
David Dorward <david@dorward.me.uk>
2025-03-21 17:59:53 +0000

    Updated screen media layout

--- client/src/components/character-info/character-info-view.tsx
+++ client/src/components/character-info/character-info-view.tsx
@@ -1,4 +1,4 @@
-import { H2 } from '@blueprintjs/core';
+import { H1 } from '@blueprintjs/core';
 import { selectUser } from '../../data/user-slice';
 import { useSelector } from 'react-redux';
 import { User } from '../../types';
@@ -10,39 +10,45 @@ const CharacterInfoView = ({ avatar, player, name, origin, codeName, motivation 
 
     return (
         <div className="character-info">
-            <img src={avatarUrl} alt="" className="avatar" />
-            <div
-                className={`header-group ${codeName ? 'hasCodeName' : 'noCodeName'}`}
-                id="group-character-name"
-            >
-                <span className="label character-name-label">Character Name</span>
-                <H2 className="field character-name-value" id="field-character-name">
-                    {name}
-                </H2>
-            </div>
-            <div className="header-group" id="group-player-name">
-                <span className="label player-name-label">Player Name</span>
-                <span className="field player-name-value" id="field-player-name">
-                    {player}
-                </span>
-            </div>
-            <div className="header-group" id="group-code-name">
-                <span className="label code-name-label">Code Name</span>
-                <H2 className="field code-name-value" id="field-code-name">
-                    {codeName}
-                </H2>
-            </div>
-            <div className="header-group" id="group-motivation">
-                <span className="label motivation-label">Motivation</span>
-                <span className="field motivation-value" id="field-motivation">
-                    {motivation}
-                </span>
-            </div>
-            <div className="header-group" id="group-origin">
-                <span className="label origin-label">Origin</span>
-                <span className="field origin-value" id="field-origin">
-                    {origin}
-                </span>
+            <H1 className="character-heading">
+                <img src={avatarUrl} alt="" className="avatar" />
+                <span>{codeName || name}</span>
+            </H1>
+            <div className="header-groups">
+                <div className="header-group" id="group-player-name">
+                    <span className="label player-name-label">Player Name</span>
+                    <span className="field player-name-value" id="field-player-name">
+                        {player}
+                    </span>
+                </div>
+
+                <div className="header-group" id="group-character-name">
+                    <span className="label character-name-label">Character Name</span>
+                    <span className="field character-name-value" id="field-character-name">
+                        {name}
+                    </span>
+                </div>
+
+                <div className="header-group" id="group-code-name">
+                    <span className="label code-name-label">Code Name</span>
+                    <span className="field code-name-value" id="field-code-name">
+                        {codeName}
+                    </span>
+                </div>
+
+                <div className="header-group" id="group-motivation">
+                    <span className="label motivation-label">Motivation</span>
+                    <span className="field motivation-value" id="field-motivation">
+                        {motivation}
+                    </span>
+                </div>
+
+                <div className="header-group" id="group-origin">
+                    <span className="label origin-label">Origin</span>
+                    <span className="field origin-value" id="field-origin">
+                        {origin}
+                    </span>
+                </div>
             </div>
         </div>
     );
