1cbc5c3736e102b477dc4ed65ac2564f6ad089d4
David Dorward <david@dorward.me.uk>
2025-03-22 07:08:12 +0000

    Fix character sheet on mobile (#702)
    * Restyle character fluff section to fit on mobile
    
    * Make the main toolbar look better on small screens

--- client/src/components/simple-dice.tsx
+++ client/src/components/simple-dice.tsx
@@ -4,25 +4,25 @@ import { d6 } from '../api/roll';
 import SkillCheck from './skill-check';
 
 type Props = {
-	hasSkillButton?: boolean;
+    hasSkillButton?: boolean;
 };
 
 const SimpleDice = ({ hasSkillButton }: Props) => {
-	return (
-		<ButtonGroup>
-			{hasSkillButton && <SkillCheck />}
+    return (
+        <ButtonGroup>
+            {hasSkillButton && <SkillCheck />}
 
-			<Button icon="random" onClick={() => d6({})}>
-				d6
-			</Button>
-			<Button icon="double-chevron-up" onClick={() => d6({ high: true })}>
-				Luck (High)
-			</Button>
-			<Button icon="double-chevron-down" onClick={() => d6({ high: false })}>
-				Luck (Low)
-			</Button>
-		</ButtonGroup>
-	);
+            <Button icon="random" onClick={() => d6({})}>
+                d6
+            </Button>
+            <Button icon="double-chevron-up" onClick={() => d6({ high: true })}>
+                Luck <span className="no-mobile">(High)</span>
+            </Button>
+            <Button icon="double-chevron-down" onClick={() => d6({ high: false })}>
+                Luck <span className="no-mobile">(Low)</span>
+            </Button>
+        </ButtonGroup>
+    );
 };
 
 export default SimpleDice;
